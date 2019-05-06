7bbeae09b4c85da83f8b898aaeb30f3fe256cd13
josepba <josep@kumori.systems>
2019-05-06 12:58:54 +0200

    Fix return type of SendRequest in request channel

--- src/channels.ts
+++ src/channels.ts
@@ -1,47 +1,47 @@
-import { EventEmitter } from 'events'
+import { EventEmitter } from "events";
 
-export type Segment = Buffer | String
-export type Message = Segment[]
+export type Segment = Buffer | String;
+export type Message = Segment[];
 
 export interface ChannelHash {
-  [index: string]: Channel
+  [index: string]: Channel;
 }
-export type Channels = Channel[]
+export type Channels = Channel[];
 
-export type MessageAsArray = Promise<[Message, Channels] | [Message]>
+export type MessageAsArray = Promise<[Message, Channels] | [Message]>;
 export interface MessageAsObject {
-  message: Message
-  dynamicChannels: Channels
+  message: Message;
+  dynamicChannels: Channels;
 }
 
 /** TargetId, used to distinguish channels connected to a
  *  complete connector, identifies a particular role in an instance.
  */
 export interface Tid {
-  iid: string
-  endpoint: string
-  service: string
+  iid: string;
+  endpoint: string;
+  service: string;
 }
 
 /**
  * Every channel is an event emitter
  */
 export interface Channel extends EventEmitter {
-  name: string
-  on (event: 'error', listener: (error: Error) => void): this
+  name: string;
+  on(event: "error", listener: (error: Error) => void): this;
 }
 
 /***********   Server functions  */
 
 // A server handler for request/reply interactions
 export interface Server {
-  (message: Message, channels?: Channels): Promise<MessageAsArray>
+  (message: Message, channels?: Channels): Promise<MessageAsArray>;
 }
 
 // Configuration when a request message is sent
 export interface RequestConfig {
-  timeout?: number
-  key?: number
+  timeout?: number;
+  key?: number;
 }
 
 /******************************* */
@@ -52,33 +52,43 @@ export interface RequestConfig {
  * Emits the following event: "destinationUnavailable"
  */
 export interface Reply extends Channel {
-  handleRequest: Server
+  handleRequest: Server;
 }
 
 export interface Request extends Channel {
-  sendRequest (message: Message, channels?: Channels, config?: RequestConfig): Promise<MessageAsArray | MessageAsObject>  // Promise fails if undeliverable
+  sendRequest(
+    message: Message,
+    channels?: Channels,
+    config?: RequestConfig
+  ): MessageAsArray | MessageAsObject; // Promise fails if undeliverable
 }
 
 export interface Receive extends Channel {
-  readonly topics: string[]
+  readonly topics: string[];
 
-  subscribe (topic: string): void
-  unsusbscribe (topic: string): void
-  on (event: 'message', listener: (message: Message, sender: Tid, channels?: Channels) => void): this
-  on (event: string, listener: Function): this
+  subscribe(topic: string): void;
+  unsusbscribe(topic: string): void;
+  on(
+    event: "message",
+    listener: (message: Message, sender: Tid, channels?: Channels) => void
+  ): this;
+  on(event: string, listener: Function): this;
 }
 
 /**
  *  Note that this is a Datagram semantics
  */
 export interface Send extends Channel {
-  send (message: Message, channels?: Channels, topic?: string): void
+  send(message: Message, channels?: Channels, topic?: string): void;
 }
 
 export interface Duplex extends Channel {
-  send (message: Message, target: Tid, channels?: Channels): void
-  getMembership (): Tid[]
-  on (event: 'message', listener: (message: Message, sender: Tid, channels?: Channels) => void): this
-  on (event: 'changeMembership', listener: (membership: Tid[]) => void): this
-  on (event: string, listener: Function): this
+  send(message: Message, target: Tid, channels?: Channels): void;
+  getMembership(): Tid[];
+  on(
+    event: "message",
+    listener: (message: Message, sender: Tid, channels?: Channels) => void
+  ): this;
+  on(event: "changeMembership", listener: (membership: Tid[]) => void): this;
+  on(event: string, listener: Function): this;
 }
